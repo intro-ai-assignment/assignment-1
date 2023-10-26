20f247238160d48aa72b2bc345084a33dafeda08
NoPro <haonp123@gmail.com>
2023-10-26 16:15:20 +0700

    modify

---
+++ annealing.py
@@ -0,0 +1,54 @@
+import random
+import math
+import time
+
+def generate_state(N):
+    return [random.randint(0, N - 1) for value in range(N)]
+
+def calc_h_value(state):
+    count = 0
+    N = len(state)
+    for i in range(N - 1):
+        for j in range(i + 1, N):
+            if state[j] == state[i] or abs(state[j] - state[i]) == abs(j - i):
+                count += 1
+    return count
+
+#Simulated Annealing
+def move_annealing(state, h_value, temp):
+    N = len(state)
+    new_state = list(state)
+    found_move = False
+    while not found_move:
+        new_state = list(state)
+        new_row = random.randint(0, N - 1)
+        new_col = random.randint(0, N - 1)
+        new_state[new_row] = new_col
+        h_cost = calc_h_value(new_state)
+        if h_cost < h_value:
+            found_move = True
+        else:
+            delta_e = h_value - h_cost
+            probability = math.exp(delta_e/temp)
+            accept_probability = min(1, probability)
+            found_move = random.random() <= accept_probability
+    return new_state
+
+def main(N):
+    print("\t--- Simulated Annealing Algorithm ---")
+    steps = 0
+    temp = N ** 2
+    annealing_rate = 0.95
+    state = generate_state(N)
+    while True:
+        steps += 1
+        h_value = calc_h_value(state)
+        print(f"+ Step: {steps} - Heuristic: {h_value}")
+        if h_value == 0:
+            print(f"Solution found in step {steps}")
+            print(state)
+            return
+        state = move_annealing(state, h_value, temp)
+        temp = max(temp*annealing_rate, 0.01)
+        if steps >= 50000:
+            break
--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-import genetic
-import hill_climbing
 import blind_searching
-
+import annealing
 import time
-
-def print_chessboard(state):
-    print()
-    N = len(state)
-    for i in range(N):
-        print(end=" ")
-        for k in range(N * 4 + 1):
-            print("-", end="")
-        print()
-        for j in range(N):
-            print(" | ", end="")
-            if state[i] == j:
-                print("Q", end="")
-            else:
-                print(" ", end="")
-        print(" |")
-    print(end=" ")
-    for k in range(N * 4 + 1):
-        print("-", end="")
-    print()
-    print()
 
 if __name__ == "__main__":
     print("\n\t*** N-Queens Problem ***")
+    print()
     N = int(input("Enter N = "))
     if N < 4:
         print("No solution...")
@@ -35,28 +13,25 @@ if __name__ == "__main__":
         print("#Select Algorithm Number to solve problem:")
         print(" 1. Breadth First Search (BFS)")
         print(" 2. Depth First Search (DFS)")
-        print(" 3. Genetic Algorithm")
-        print(" 4. Steepest-Ascent Hill-Climbing")
-        print(" 5. Simulated Annealing")
-        select = int(input("Enter number: "))
-        if select < 1 or select > 5:
-            print("Please enter valid value...")
+        print(" 3. Simulated Annealing (Heuristic)")
+        select = input("Enter number: ")
+        if len(select) > 1:
+            print("\n\t***Please enter valid value...\n")
         else:
-            t1 = time.time()
-            result = []
-            if select == 1:
-                blind_searching.BFS(N)
-            elif select == 2:
-                blind_searching.DFS(N)
-            elif select == 3:
-                result = genetic.genetic_alg(N)
-            elif select == 4:
-                result = hill_climbing.steepest_ascent(N)
+            select = int(select)
+            if select < 1 or select > 3:
+                print("\n\t***Please enter valid value...\n")
             else:
-                result = hill_climbing.annealing(N)
-            t2 = time.time()
-            if len(result) > 0 and len(result) < 30:
-                print_chessboard(result)
-            print("Time cost:", t2 - t1, "seconds")
-            break
+                t1 = time.time()
+                result = []
+                if select == 1:
+                    blind_searching.BFS(N)
+                elif select == 2:
+                    blind_searching.DFS(N)
+                else:
+                    annealing.main(N)
+                t2 = time.time()
+                print("Time cost:", t2 - t1, "seconds")
+                break
+        
 
